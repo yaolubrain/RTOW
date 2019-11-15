8803b461e0dcdfaf9e626746cea483f75bf06ed4
yao <yaolubrain@gmail.com>
2019-11-15 17:46:37 +1100

    finished

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 
 #include "vec3.h"
 #include "ray.h"
-#include "camera.h"
 #include "hitable.h"
 #include "material.h"
+#include "camera.h"
 
 
 vec3 color(const ray& r, hitable *world, int depth) {
@@ -26,27 +26,67 @@ vec3 color(const ray& r, hitable *world, int depth) {
   }
 }
 
+hitable *random_scene() {
+  int n = 110;
+  hitable **list = new hitable*[n+1];
+  list[0] = new sphere(vec3(0,-1000,0), 1000, new lambertian(vec3(0.5,0.5,0.5)));
+  int i = 1;
+
+  for (int a = -5; a < 5; a++) {
+    for (int b = -5; b < 5; b++) {
+      float choose_mat = rand_uni(gen);
+      vec3 center(a+0.9*rand_uni(gen), 0.2, b+0.9*rand_uni(gen));
+      if ((center - vec3(4,0.2,0)).length() > 0.9) {
+        if (choose_mat < 0.8) {
+          list[i++] = new sphere(center, 0.2, new lambertian(vec3(rand_uni(gen)*rand_uni(gen),
+                                                                  rand_uni(gen)*rand_uni(gen),
+                                                                  rand_uni(gen)*rand_uni(gen))));
+        } else if (choose_mat < 0.95) {
+          list[i++] = new sphere(center, 0.2, new metal(vec3(0.5*(1+rand_uni(gen)),
+                                                             0.5*(1+rand_uni(gen)),
+                                                             0.5*(1+rand_uni(gen))), 
+                                                        0.5*rand_uni(gen)));
+        } else {
+          list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+        }
+      }
+    }
+  }
+
+  list[i++] = new sphere(vec3(0,1,0),  1, new dielectric(1.5));
+  list[i++] = new sphere(vec3(-4,1,0), 1, new lambertian(vec3(0.4,0.2,0.1)));
+  list[i++] = new sphere(vec3(4,1,0), 1, new metal(vec3(0.7,0.6,0.5), 0));
+  return new hitable_list(list, i);
+}
+
 
 int main() {
   
-  int nx = 800;
-  int ny = 400;
-  int ns = 10;
+  int nx = 1200;
+  int ny = 600;
+  int ns = 100;
 
   std::ofstream fs;
   fs.open("ray_tracing.ppm");
 
   fs << "P3\n" << nx << " " << ny << "\n255\n";
 
+//  hitable *list[5];
+//  list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.1,0.2,0.5)));
+//  list[1] = new sphere(vec3(0,-100.5,-1), 100, new lambertian(vec3(0.8,0.8,0.0)));
+//  list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8,0.6,0.2), 0.3));
+//  list[3] = new sphere(vec3(-1,0,-1), 0.5, new dielectric(1.5));
+//  list[4] = new sphere(vec3(-1,0,-1), -0.45, new dielectric(1.5));
+//  hitable *world = new hitable_list(list,5);
+  hitable *world = random_scene();
+  
 
-  hitable *list[4];
-  list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.8,0.3,0.3)));
-  list[1] = new sphere(vec3(0,-100.5,-1), 100, new lambertian(vec3(0.8,0.8,0.0)));
-  list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8,0.6,0.2), 0.9));
-  list[3] = new sphere(vec3(-1,0,-1), 0.5, new metal(vec3(0.8,0.8,0.8), 0.01));
-  hitable *world = new hitable_list(list,4);
-
-  camera cam;
+  //camera cam(90, float(nx)/float(ny));
+  vec3 lookfrom(25,2.5,10);
+  vec3 lookat(0,0,0);
+  float dist_to_focus = (lookfrom-lookat).length();
+  float aperture = 0.5;
+  camera cam(lookfrom, lookat, vec3(0,1,0), 10, float(nx)/float(ny), aperture, dist_to_focus);
 
 
   for (int j = ny-1; j >= 0; j--) {
@@ -70,7 +110,9 @@ int main() {
       int ib = int(255.99*col[2]);
 
       fs << ir << " " << ig << " " << ib << "\n";
+
     }
+    std::cout << j << std::endl;
   }
 
   fs.close();
--- material.h
+++ material.h
@@ -12,6 +12,24 @@ vec3 reflect(const vec3& v, const vec3& n) {
   return v - 2*dot(v,n)*n;
 }
 
+bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted) {
+  vec3 uv = unit_vector(v);
+  float dt = dot(uv, n);
+  float discriminant = 1.0 - ni_over_nt*ni_over_nt*(1-dt*dt);
+  if (discriminant > 0) {
+    refracted = ni_over_nt*(uv - n*dt) - n*sqrt(discriminant);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+float schlick(float cosine, float ref_idx) {
+  float r0 = (1-ref_idx) / (1+ref_idx);
+  r0 = r0*r0;
+  return r0 + (1-r0)*pow((1-cosine), 5);
+}
+
 vec3 random_in_unit_sphere() {
   vec3 p;
   do {
@@ -30,7 +48,7 @@ class lambertian : public material {
  public:
   lambertian(const vec3&a ) : albedo(a) {};
   virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
-    vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+    vec3 target = rec.p + rec.normal + 0*random_in_unit_sphere();
     scattered = ray(rec.p, target-rec.p);
     attenuation = albedo;
     return true;
@@ -49,6 +67,46 @@ class metal : public material {
   }
   vec3 albedo;
   float fuzz;
+};
+
+class dielectric : public material {
+ public:
+  dielectric(float ri) : ref_idx(ri) {};
+  virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
+    vec3 outward_normal;
+    vec3 reflected = reflect(r_in.direction(), rec.normal);
+    float ni_over_nt;
+    attenuation = vec3(1,1,1);
+    vec3 refracted;
+    float reflect_prob;
+    float cosine;
+    if (dot(r_in.direction(), rec.normal) > 0) {
+      outward_normal = -rec.normal;
+      ni_over_nt = ref_idx;
+      cosine = ref_idx * dot(r_in.direction(), rec.normal) / r_in.direction().length();
+    } else {
+      outward_normal = rec.normal;
+      ni_over_nt = 1.0 / ref_idx;
+      cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
+    }
+
+    if (refract(r_in.direction() , outward_normal, ni_over_nt, refracted)) {
+      reflect_prob = schlick(cosine, ref_idx);
+    } else {
+      scattered = ray(rec.p, refracted);
+      reflect_prob = 1.0;
+    }
+    
+    if (rand_uni(gen) < reflect_prob) {
+      scattered = ray(rec.p, reflected);
+    } else {
+      scattered = ray(rec.p, refracted);
+    }
+
+    return true;
+  };
+
+  float ref_idx;
 };
 
 
